b2a22c47c543086588ae5f43074aac57471734fe
Warren <warren.buch16@gmail.com>
2022-07-17 12:16:19 +0200

    Last commit, I finished the generate button by putting all the request inside a javascript function

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,22 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.adviceslip.com/advice')
-
-request.responseType = 'json'
-
-request.onload = function () 
+function generate()
 {
-    const quote = request.response
-    var advice = document.getElementById("advice")
-    var text = document.getElementById("quote")
-    advice.innerHTML = "Advice #" + quote.slip.id
-    text.innerHTML = '\"' + quote.slip.advice + '\"'
-}
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest()
 
-// Send request
-request.send()
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://api.adviceslip.com/advice')
+
+    request.responseType = 'json'
+
+    request.onload = function() 
+    {
+        const quote = request.response
+        var advice = document.getElementById("advice")
+        var text = document.getElementById("quote")
+        advice.innerHTML = "Advice #" + quote.slip.id
+        text.innerHTML = '\"' + quote.slip.advice + '\"'
+    }
+
+    // Send request
+    request.send()
+}
